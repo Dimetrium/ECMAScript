573370be92f5b9124d7a0d358fc1efd4b01d2a39
Dimitry <diskakun@gmail.com>
2015-03-23 11:16:17 +0200

    Complit

--- task-7/js/getData.js
+++ task-7/js/getData.js
@@ -1,24 +1,16 @@
-// TODO: Сделать проверку на наличие поста 
-//       и вывод сообщении о том что такой пост есть.
-
 var getData = {
     root: 'http://jsonplaceholder.typicode.com',
     archive: {},
     localStorCurrKey: null,
-    refresCategoryFlag: null,
-    
-    // Draw Table from localStorage
+    refreshCategoryFlag: null,
     drawTable: function () {
         var dynamicColumns,
             dynamicRows;
 
-        // Get dynamic Columns and Rows
         if (0 == this.localStorCurrKey) {
-            // Get Column Header
             $.each(this.archive[0][0], function (key) {
                 return dynamicColumns += "<th>" + key + "</th>";
             });
-            // Get Rows
             $.each(this.archive[0], function () {
                 dynamicRows += "<tr>";
                 $.each(this, function (kay, value) {
@@ -28,11 +20,9 @@ var getData = {
                 return dynamicRows;
             });
         } else {
-            // Get Column Header
             $.each(this.archive[0], function (key) {
                 return dynamicColumns += "<th>" + key + "</th>";
             });
-            // Get Rows
             $.each(this.archive, function () {
                 dynamicRows += "<tr>";
                 $.each(this, function (kay, value) {
@@ -42,67 +32,55 @@ var getData = {
                 return dynamicRows;
             });
         }
-        // Draw Table Header
-
         $("#table").html('<thead><tr>' + dynamicColumns + '</tr></thead>');
-        // Draw Table Rows
         $("#table").append('<tbody><tr>' + dynamicRows + '</tr></tbody>');
-
-
     },
-    // Get Object from LocalStore
     getObjFromLocalStore: function () {
         var keys = Object.keys(localStorage),
             key;
 
-        if (1 == this.refresCategoryFlag) {
+        if (true == this.refreshCategoryFlag) {
             this.archive = {};
         }
         if (null !== localStorage.key(0)) {
             this.localStorCurrKey = localStorage.key(0).replace(/\:.*/, '');
             for (var i = 0; key = keys[i]; i++) {
                 this.archive[i] = JSON.parse(localStorage.getItem(key));
-            }console.log('getObj1 true');
+            }
             getData.drawTable();
         } else {
-            console.log('getObj false');
             return false;
         }
     },
-    // Get value from JSONplaceholder and set to LocalStorage
     setToLocalStorage: function (id, category) {
-            var root = this.root;
-            $.ajax({
-                url: root + '/' + category + '/' + id,
-                method: 'GET'
-            }).then(function (data) {
-                var currentCategory = category,
-                    currentId = id,
-                    _this = this;
-                // Если уже есть записи в локалсторе, 
-                // записываем их значения в переменные 
-                // для последнующего исспользвоания.
-                // Если нет - то переменные по умолчанию те,
-                // что переданные с Input Fields
-                if (null !== localStorage.key(0)) {
-                    currentCategory = localStorage.key(0).replace(/.*\:/, '');
-                    currentId = localStorage.key(0).replace(/\:.*/, '')
-                }
-                // Если пришла пустая строка, или если сменилась категория
-                // делаем очитску локалстореджа
-                if ('' == id) {
-                    id = 0;
-                    localStorage.clear();
-                } else if (currentCategory !== category) {
-                    this.refresCategoryFlag = 1;
-                    localStorage.clear();
-                } else if (0 == currentId) {
-                    localStorage.clear();
-                }
-                // Парсим в локалстор...
-                localStorage[id + ':' + category] = JSON.stringify(data);
-                getData.getObjFromLocalStore();
-                console.log(' 0 Putting in localStorage');
-            });
+        var root = this.root,
+            _this = this;
+        if (false == jQuery.isEmptyObject(localStorage[id + ':' + category])) {
+            return $("#alert").html('<div class="alert alert-warning" role="alert">this id: ' + id + ' in list</div>');
+        } else {
+            $("#alert").html('');
         }
+        $.ajax({
+            url: root + '/' + category + '/' + id,
+            method: 'GET'
+        }).then(function (data) {
+            var currentCategory = category,
+                currentId = id;
+            if (null !== localStorage.key(0)) {
+                currentCategory = localStorage.key(0).replace(/.*\:/, '');
+                currentId = localStorage.key(0).replace(/\:.*/, '')
+            }
+            if ('' == id) {
+                id = 0;
+                localStorage.clear();
+            } else if (currentCategory !== category) {
+                _this.refreshCategoryFlag = true;
+                localStorage.clear();
+            } else if (0 == currentId) {
+                localStorage.clear();
+            }
+            localStorage[id + ':' + category] = JSON.stringify(data);
+            getData.getObjFromLocalStore();
+        });
+    }
 };
--- task-7/js/script.js
+++ task-7/js/script.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
-
     getData.getObjFromLocalStore();
-//    getData.drawTable();
-
     $("#button").click(function () {
         var id = $('#input').val();
         var category = $('#category').val();
         getData.setToLocalStorage(id, category);
-//            jsData.getObjFromLocalStore();
-
     });
 });
